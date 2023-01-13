cabdaf8262863e8e88efedc5b95cc30c48324bd3
Nafeu Nasir <nafeu.nasir@gmail.com>
2023-01-12 19:46:10 -0500

    Initial commit.

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import './app.css';
+import Footer from './components/footer';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Hello World.</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/footer/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.css';
+
+const Footer = () => (
+  <div className="footer">Made by <a href="#YOUR_SITE_URL">[YOUR_FULL_NAME]</a></div>
+);
+
+export default Footer;
